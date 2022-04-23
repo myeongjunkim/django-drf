430feb04000a5657773283c906ebf00774dba3d9
myeongjunkim <ilikerunning@naver.com>
2022-04-23 20:07:59 +0900

    feat: admin custom

--- demos/admin.py
+++ demos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'content', 'created_at', 'view_count', 'writer')
+    list_filter = ('created_at',)
+    search_fields = ('id', 'writer')
+    search_help_text = '게시판 번호, 작성자 검색이 가능합니다.'
+
+admin.site.register(Comment)
--- demos/models.py
+++ demos/models.py
@@ -1,3 +1,20 @@
+from re import T
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    view_count = models.IntegerField(default=0)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
