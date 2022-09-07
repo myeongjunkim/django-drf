c8d406099293138b75deaeae112ef379a366d66d
myeongjunkim <ilikerunning@naver.com>
2022-09-07 18:00:50 +0900

    [todo] feat:CRUD API

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Todo
 
+
 class TodoSimpleSerializer(serializers.ModelSerializer):
-    class Mata:
+    class Meta:
         model =  Todo
-        field =  ('id', 'title', 'description', 'complete', 'important')
+        fields =  ('id', 'title', 'description', 'complete', 'important')
+
+
+class TodoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Todo
+        fields =  ('id', 'title', 'description', 'created', 'complete', 'important')
+
+
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Todo
+        fields =  ('title', 'description', 'important')
--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,9 @@ from rest_framework import routers
 router = routers.SimpleRouter()
 
 urlpatterns = [
-    
+    path('todo/',TodosAPI.as_view()),
+    path('todo/<int:pk>/',TodoAPI.as_view()),
+    path('done/',DoneTodosAPI.as_view()),
+    path('done/<int:pk>/',DoneTodoAPI.as_view()),
 
 ]
